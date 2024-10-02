b1f459e94c56ff89210d948b52b954c58703d2bd
peter-lyr <llydrp.ldp@gmail.com>
2024-10-03 02:08:59 +0800

    [p/002-python]: [pys]:09-change-input-method.py

--- lua/f.lua
+++ lua/f.lua
@@ -1743,6 +1743,42 @@ function M.git_commit_push_recursive_quick(which)
   M.git_push_recursive_do(commit, nil, { 'commit', 'push', })
 end
 
+M.input_method_py = M.get_py '09-change-input-method.py'
+
+function M.change_language(lang)
+  M.cmd([[silent !start /b /min cmd /c "%s %s"]], M.input_method_py, lang)
+end
+
+M.aucmd('ModeChanged', 'inputmethod.ModeChanged', {
+  callback = function()
+    if M.in_arr(vim.fn.mode(), { 'c', 'i', 't', 'r', 'R', }) then
+      M.change_language 'ZH'
+    else
+      M.change_language 'EN'
+    end
+  end,
+})
+
+M.aucmd('CmdlineLeave', 'inputmethod.CmdlineLeave', {
+  callback = function()
+    if M.in_arr(vim.fn.mode(), { 'i', 't', 'r', 'R', }) then
+      M.change_language 'ZH'
+    else
+      M.change_language 'EN'
+    end
+  end,
+})
+
+M.aucmd('FocusLost', 'inputmethod.FocusLost', {
+  callback = function()
+    if M.in_arr(vim.fn.mode(), { 'c', 'i', 't', 'r', 'R', }) then
+      M.write_lines_to_file({ '1', }, [[C:\Windows\Temp\nvim-qt.exe-input-method.txt]])
+    else
+      M.write_lines_to_file({ '0', }, [[C:\Windows\Temp\nvim-qt.exe-input-method.txt]])
+    end
+  end,
+})
+
 M.clone_if_not_exist 'org'
 
 return M
