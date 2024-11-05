e4a2712d39a8a70a2a5bd8098bbd33477d813383
peter-lyr <llydrp.ldp@gmail.com>
2024-11-05 20:07:55 +0800

    local temp_3 = vim.inspect(file) .. vim.inspect(params) .. vim.inspect(opts)

--- lua/f.lua
+++ lua/f.lua
@@ -448,7 +448,7 @@ function M.run_py_get_cmd(file, params, opts)
       vim.fn.system(M.format('move /y "%s" "%s\\run-cmd-%s"', RunCmdDir, RunCmdOldDir, vim.fn.strftime '%Y%m%d-%H%M%S'))
       vim.fn.mkdir(RunCmdDir)
     end
-    local temp_3 = tostring(file) .. tostring(params) .. tostring(opts)
+    local temp_3 = vim.inspect(file) .. vim.inspect(params) .. vim.inspect(opts)
     print('===', temp_3)
     local temp_2 = M.complex_string_hash(temp_3)
     if not M.in_arr(temp_2, Sta_234_cnts) then
