3cd48a294b30c750d55959aea7bda7a2c0242a01
peter-lyr <llydrp.ldp@gmail.com>
2025-01-17 16:19:42 +0800

    M.hi_winbar()

--- lua/f.lua
+++ lua/f.lua
@@ -47,6 +47,9 @@ function M.hi_winbar()
 end
 
 M.hi_winbar()
+vim.fn.timer_start(5000, function()
+  M.hi_winbar()
+end, { ['repeat'] = 1, })
 
 function M.get_win_buf_nrs()
   local buf_nrs = {}
@@ -1492,6 +1495,7 @@ function M.start_nvim_qt(file)
   if M.in_str('session', file) then
     if M.in_str('!', file) then
       vim.cmd 'SessionsSave!'
+      -- M.run__silent(M.format("sed -i -e '/winbar/d' %s", SessionVim))
     end
     local exe = vim.fn.system(string.format('tasklist /FI "PID eq %d"', vim.loop.os_getppid()))
     if string.find(exe, 'nvim%-qt.exe') then
@@ -2784,6 +2788,7 @@ function M.save_sessions_at_cwd_do(project_root)
   M.write_lines_to_file(session_saved_projects, M.session_saved_projects_txt)
   -- M.run__silent(M.format('copy /y "%s" "%s"', SessionVim, M.get_file({ project_root, }, session_vim)))
   M.cmd('mksession %s', M.get_file({ project_root, }, session_vim))
+  -- M.run__silent(M.format("sed -i -e '/winbar/d' %s", session_vim))
 end
 
 function M.save_sessions_at_cwd()
